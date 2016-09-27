0698335afcd7f16093963957b3293429bf49a06b
joao_felipe <joaofelipe1294@gmail.com>
2016-09-27 15:33:54 -0300

    Melhorada segmentacao, agora tem apenas 3 imagens com erro

--- dev.py
+++ dev.py
@@ -70,6 +70,68 @@ def hsi_chanels(rgb_image, chanel_return = 'S'):
 	return chanel
 
 
+def otsu_threshold(image):
+	blur_image = cv2.GaussianBlur(saturation,(5,5),0)
+   	otsu_image = cv2.threshold(blur_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+   	return otsu_image
+
+
+def get_number_of_objects(image):
+	img , contours, hierarchy = cv2.findContours(image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	return len(contours)
+
+def find_interest_cell(image):
+	image_center_point = tuple([int(opening.shape[0] / 2) , int(opening.shape[1]) / 2])
+	img , contours, hierarchy = cv2.findContours(image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	lowest_index = 0
+	lowest_distance = None
+	for contour_index in range(0 , len(contours)):
+		(x,y),cell_radius = cv2.minEnclosingCircle(contours[contour_index])
+		distance_to_center = math.sqrt(math.pow((x - image_center_point[1]) , 2) + math.pow(y - image_center_point[1] , 2))
+		if lowest_distance == None or distance_to_center < lowest_distance:
+			lowest_index = contour_index
+			lowest_distance = distance_to_center
+	(x,y),cell_radius = cv2.minEnclosingCircle(contours[lowest_index])
+	cell_radius = int(cell_radius)
+	contours.pop(lowest_index)
+	cell_center = (int(x),int(y))
+	"""mask = flood(img , 255 , single_seed = cell_center)
+	for contour in contours:
+		(x,y),object_radius = cv2.minEnclosingCircle(contour)
+		object_radius = int(object_radius)
+		object_center = (int(x),int(y))
+		distance_to_cell_center = math.sqrt(math.pow((x - cell_center[1]) , 2) + math.pow(y - cell_center[1] , 2))
+		if distance_to_cell_center < cell_radius:
+			mask = opening.copy()
+			break
+			#mask = flood(mask , 255 , single_seed = object_center)						
+	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3) , np.uint8))
+	cv2.circle(grayscale_image , cell_center , cell_radius , 255 , 1):"""
+	return cell_center , cell_radius , contours
+
+
+def define_mask(image , contours , cell_center , cell_radius):
+	mask = flood(image , 255 , single_seed = cell_center)
+	contours_valids = []
+	for contour in contours:
+		(x,y),object_radius = cv2.minEnclosingCircle(contour)
+		object_radius = int(object_radius)
+		object_center = (int(x),int(y))
+		distance_to_cell_center = math.sqrt(math.pow((x - cell_center[1]) , 2) + math.pow(y - cell_center[1] , 2))
+		cv2.circle(grayscale_image , cell_center , cell_radius , 255 , 1)
+		print("DISTANCIA DO CENTRO : " + str(distance_to_cell_center))
+		print("RAIO DA CELULA : " + str(cell_radius))
+		if distance_to_cell_center > cell_radius:
+			#mask = opening.copy()
+			mask = flood(mask , 0 , single_seed = object_center)
+		else:
+			mask = flood(mask , 255 , single_seed = object_center)						
+	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3) , np.uint8))
+	cv2.circle(grayscale_image , cell_center , cell_radius , 255 , 1)
+	return mask
+
+
+
 
 base = BaseLoader()
 base.load('teste')
@@ -80,114 +142,27 @@ for image in base.images:
 	rgb_image = cv2.imread(image.path)
 	grayscale_image = cv2.imread(image.path ,cv2.IMREAD_GRAYSCALE)
 	saturation = hsi_chanels(rgb_image)
-	blur_image = cv2.GaussianBlur(saturation,(5,5),0)
-   	otsu_image = cv2.threshold(blur_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+   	otsu_image = otsu_threshold(saturation)
 	flooded_image = flood(otsu_image)
 	opening = cv2.morphologyEx(flooded_image, cv2.MORPH_OPEN, np.ones((5,5) , np.uint8))
 	img , contours, hierarchy = cv2.findContours(opening.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	cv2.drawContours(img, contours, -1,255, 1)
 	image_center_point = tuple([int(opening.shape[0] / 2) , int(opening.shape[1]) / 2])
-	if len(contours) > 1:
-		lowest_index = 0
-		lowest_distance = None
-		for contour_index in range(0 , len(contours)):
-			(x,y),cell_radius = cv2.minEnclosingCircle(contours[contour_index])
-			distance_to_center = math.sqrt(math.pow((x - image_center_point[1]) , 2) + math.pow(y - image_center_point[1] , 2))
-			if lowest_distance == None or distance_to_center < lowest_distance:
-				lowest_index = contour_index
-				lowest_distance = distance_to_center
-		(x,y),cell_radius = cv2.minEnclosingCircle(contours[lowest_index])
-		cell_radius = int(cell_radius)
-		contours.pop(lowest_index)
+	if get_number_of_objects(opening) > 1:
+		cell_center , cell_radius , contours = find_interest_cell(opening)
+		if cell_radius < 10:
+			mask = opening.copy()
+		else:
+			mask = define_mask(img , contours , cell_center , cell_radius)
+		
 	else:
-		(x,y),cell_radius = cv2.minEnclosingCircle(contours[0])
-	cell_center = (int(x),int(y))
-	copy = np.zeros((opening.shape) , np.uint8)
-	mask = flood(img , 255 , single_seed = cell_center)
-	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3) , np.uint8))
-
-		#print(len(contours))
-		#for contour in contours:
-			#cv2.drawContours(edges, contours, -1, (0,255,0), 3)
-			#(x,y),object_radius = cv2.minEnclosingCircle(contour)
-			#object_center = (int(x),int(y))
-			#object_radius = int(object_radius)
-			#print("========================================")
-			#print("CELL CENTER : " + str(cell_center))
-			#print("CELL RADIUS : " + str(cell_radius))
-			#print("OBJECT CENTER : " + str(object_center))
-			#print("OBJECT RADIUS : " + str(object_radius))
-			#print(object_center[0] > cell_center[0] + cell_radius) 
-			#print(object_center[0] < cell_center[0] - cell_radius)
-			#print(object_center[1] > cell_center[1] + cell_radius or object_center[1] < cell_center[1] - cell_radius)
-			#print("========================================")
-			#opening.itemset(cell_center , 0)
-			#if (object_center[0] > cell_center[0] + cell_radius or object_center[0] < cell_center[0] - cell_radius) and (object_center[1] > cell_center[1] + cell_radius or object_center[1] < cell_center[1] - cell_radius):
-			#	cv2.circle(opening,object_center,object_radius + 5,0,-1)
-		#(x,y),radius = cv2.minEnclosingCircle(contours[lowest_index])
-		#center = (int(x),int(y))
-		#radius = int(radius)
-		#cv2.circle(rgb_image,cell_center,cell_radius,(0,255,0),2)
-		#cv2.imshow('rgb' , rgb_image)
-		#cv2.waitKey(0)"""
-
-	cv2.imshow('opening' , opening)
-	cv2.imshow('mask' , mask)
-	#cv2.imshow('original' , rgb_image )
-	cv2.imshow('img' , img)
-	cv2.waitKey(0)
-	#rgb_chanels(rgb_image)
-	#show_histogram(image.path)
-	#cv2.imshow('image' ,cv2.imread( image.path))
-	#cv2.waitKey(250)
-	#original_image = cv2.imread( image.path, 0)
-	#equalized_image = cv2.equalizeHist(original_image)
-	#flooded_image = flood(original_image)
-	#laplacian = cv2.Laplacian(flooded_image , cv2.CV_8U)
-	#edges = cv2.Canny(flooded_image,10,200)
-	#erosion = cv2.erode(cv2.bitwise_not(edges),np.ones((2,2) , np.uint),iterations = 1)
-	#dilation = cv2.dilate(flooded_image,np.ones((3,3) , np.uint),iterations = 1)
-	#closing = cv2.morphologyEx(flooded_image, cv2.MORPH_CLOSE, np.ones((3,3) , np.uint8))
-	#show = np.concatenate((original_image , equalized_image & erosion ,flooded_image & erosion , dilation , closing ) , axis=1)
-	#cv2.imshow('resault' , show)
-	#cv2.waitKey(0)"""
-
-
-""" mostra todas as imagens pre processadas ""
-for cell in base_loader.cells:
-	gray_scale_image = cv2.imread(cell.image_path , 0)
-	floodfill_image = gray_scale_image.copy()
-	h, w = floodfill_image.shape[:2]                                         
-	mask = np.zeros((h + 2 , w + 2) , np.uint8)                              
-	print(cell.image_id)
-	seeds = []
-	for x in xrange(0 , 230 , 5):
-		seeds.append(tuple([0 , x]))
-		seeds.append(tuple([230 , x]))
-		seeds.append(tuple([x , 0]))
-		seeds.append(tuple([x , 230]))
-	for seed in seeds:
-		cv2.floodFill(floodfill_image , mask , seed , 0 , loDiff = 3 , upDiff = 3)
-	show = np.concatenate((gray_scale_image , floodfill_image) , axis=1)
+		mask = opening.copy()
+	#cv2.imshow('opening' , opening)
+	#cv2.imshow('mask' , mask)
+	#cv2.imshow('img' , img)
+	#cv2.waitKey(0)
+	show = np.concatenate((grayscale_image , opening , img , mask) , axis=1)
 	cv2.imshow('resault' , show)
-	cv2.waitKey(250)
-""""""""""""""""""""""""""""""""""""""""""
-rgb_image = cv2.imread(image_path)
-gray_scale_image = cv2.imread(image_path , 0)
-floodfill_image = gray_scale_image.copy()
-h, w = floodfill_image.shape[:2]                                         
-mask = np.zeros((h + 2 , w + 2) , np.uint8)                              
-print(mask.shape)
-seeds = []
-for x in xrange(0 , 259 , 5):
-	seeds.append(tuple([0 , x]))
-	seeds.append(tuple([255 , x]))
-	seeds.append(tuple([x , 0]))
-	seeds.append(tuple([x , 255]))
-for seed in seeds:
-	cv2.floodFill(floodfill_image , mask , seed , 255 , loDiff = 4 , upDiff = 4)
+	cv2.waitKey(0)
 
-#cv2.imshow('gray' , gray_scale_image)
-cv2.imshow('flooded image' , floodfill_image)
-cv2.waitKey(0)"""
-
+	
