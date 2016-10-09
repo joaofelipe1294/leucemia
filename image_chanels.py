6c8ce1a21d86012f6bc0b4f5bcd290b8f6e98ad0
joao_felipe <joaofelipe1294@gmail.com>
2016-10-09 20:58:11 -0300

    Refatorado metodo que trabalha com os canais HSV

--- image_chanels.py
+++ image_chanels.py
@@ -36,48 +36,24 @@ class ImageChanels(object):
 
 
 	def hsv(self, chanel = None , display = False):
-		#trabalha com canais HSI
-		chanel_index = -1
+		#trabalha com canais HSV
+		hsv_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2HSV)
 		if chanel:                           #trabalha com apenas um canal
 			if chanel == 'H':
 				chanel_index = 0
 			elif chanel == 'S':
 				chanel_index = 1
-			elif chanel_index == 'V':
-				chanel = 2
-			hsi_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2HSV)
-			chanel_image = np.zeros((self.height , self.width) , np.uint8)
-			for line in xrange(0 , self.height):
-				for col in xrange(0 , self.width):
-					val = hsi_image.item( line, col, chanel_index)
-					chanel_image.itemset((line , col), val)
+			elif chanel == 'V':
+				chanel_index = 2
+			image_chanel = cv2.split(hsv_image)[chanel_index]
 			if display:
-				cv2.imshow('chanel' , chanel_image)
+				cv2.imshow('image_chanel' , image_chanel)
 				cv2.waitKey(0)
-			return chanel_image
-		else:                               #trabalha com todos os canais
-			hsi_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2HSV)
-			hue_chanel = np.zeros((self.height , self.width) , np.uint8)
-			saturation_chanel = np.zeros((self.height , self.width) , np.uint8)
-			intensity_chanel = np.zeros((self.height , self.width) , np.uint8)
-			chanels = [hue_chanel , saturation_chanel , intensity_chanel]
-			for chanel_index in range(0,3):
-				for line in xrange(0,self.height):
-					for col in xrange(0,self.width):
-						value = hsi_image.item(line , col , chanel_index)
-						chanels[chanel_index].itemset((line , col) , value)
+			return image_chanel
+		else:
+			h , s , v = cv2.split(hsv_image)
 			if display:
-				show = np.concatenate(( hue_chanel , saturation_chanel , intensity_chanel) , axis=1)
+				show = np.concatenate(( h , s , v) , axis=1)
 				cv2.imshow('resault' , show)
-				cv2.waitKey(0)		
-			return 	hue_chanel , saturation_chanel , intensity_chanel
-
-
-
-image = cv2.imread('ALL_IDB2/img/Im001_1.tif')
-#cv2.imshow('image' , image)
-#cv2.waitKey(0)
-r , g , b = ImageChanels(image).rgb(display = True)
-show = np.concatenate(( r , g , b) , axis=1)
-cv2.imshow('resault' , show)
-cv2.waitKey(0)				
+				cv2.waitKey(0)
+			return h , s , v		
