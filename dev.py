4cfdf898b07a8e41b8deb18f4e9f6ccbcc3bf282
joao_felipe <joaofelipe1294@gmail.com>
2016-10-13 19:08:21 -0300

    Concluido salvamento e carregamento dos dados em arquivos de cahe

--- dev.py
+++ dev.py
@@ -2,26 +2,30 @@ from modules.base.base_loader import BaseLoader
 from modules.machine_learning.classifier import Classifier
 from modules.machine_learning.merge_classifiers import MergeClassifiers
 from modules.base.base_processor import BaseProcessor
+from modules.utils.file_handler import FileHandler
 
 
-base = BaseLoader(train_base_path = 'bases/ALL_IDB2/img' ,  validation_base_path = 'bases/Teste_ALL_IDB2/ALL')
-base.load()
-BaseProcessor().load(images = base.train_images , train = True)
-print()
-BaseProcessor().load(images = base.validation_images , validation = True)
+#base = BaseLoader(train_base_path = 'bases/ALL_IDB2/img' ,  validation_base_path = 'bases/Teste_ALL_IDB2/ALL')
+#base.load()
+#BaseProcessor().load(images = base.train_images , train = True)
+#print()
+#BaseProcessor().load(images = base.validation_images , validation = True)
 
 
-X = base.train_vectors
-y = base.train_labels
+#X = base.train_vectors
+#y = base.train_labels
+X , y = FileHandler().load_vectors_and_labels(train = True)
+X_ , y_ = FileHandler().load_vectors_and_labels(validation = True)
 
+#classes = MergeClassifiers(X , y , X_).vote('SVM' , 'LDA' , 'TREE')
+classes = Classifier(X , y , X_).lda()
 
-classes = MergeClassifiers(X , y , base.valid_vectors).vote('SVM' , 'LDA' , 'TREE')
 corrects = 0
 errors = 0
 fn = 0
 fp = 0
 for index in xrange(0 , len(classes)):
-	if classes[index] == base.validation_labels[index]:
+	if classes[index] == y_[index]:
 		corrects += 1
 	else:
 		if classes[index] == 1:
@@ -29,7 +33,7 @@ for index in xrange(0 , len(classes)):
 		elif classes[index] == 0:
 			fp += 1
 		errors += 1
-percentage = (corrects * 100) / len(base.valid_vectors)
+percentage = (corrects * 100) / len(X_)
 print('+%-15s+%-15s+%-15s+%-15s+' % ('-' * 15 , '-' * 15 , '-' * 15 , '-' * 15))
 print('|%-15s|%-15s|%-15s|%-15s|' % (' ' * 15 , 'PRECISION_%' , 'FALSE_POSITIVE' , 'FALSE_NEGATIVE'))
 print('+%-15s+%-15s+%-15s+%-15s+' % ('-' * 15 , '-' * 15 , '-' * 15 , '-' * 15))
--- modules/utils/file_handler.py
+++ modules/utils/file_handler.py
@@ -1,17 +1,23 @@
 class FileHandler:
 
-	def __init__(self)
+	def __init__(self):
 		self.train_file = 'train_data.txt'
 		self.validation_file = 'validation_data.txt'
 
-	def get_vetctors_and_labels(self , train = False , validation = False):
+
+	def load_vectors_and_labels(self , train = False , validation = False):
+		vectors = []
+		labels = []
 		file_path = ''
 		if train:
 			file_path = self.train_file
 		elif validation:
 			file_path = self.validation_file
 		file = open(file_path , 'r')
-		
 		for line in file:
-			if train:
-				#continuar a carregar dados dos arquivos
+			values = map(int, line.split(','))
+			label = values.pop(len(values) - 1)
+			vectors.append(values)
+			labels.append(label)
+		return vectors , labels
+				
