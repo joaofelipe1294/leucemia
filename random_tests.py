17eb3c2410f74f9b29a3f6e53aac2bf89abb1265
Joao Felipe Lopes <joaofelipe1294@gmail.com>
2016-12-07 15:24:19 -0200

    Implementado normalizador MINMAX

--- random_tests.py
+++ random_tests.py
@@ -5,33 +5,90 @@ from modules.image_processing.segmentation import SmartSegmentation
 from modules.utils.progress_bar import ProgressBar
 
 
-base = BaseLoader(train_base_path = 'bases/ALL' ,  validation_base_path = 'bases/Teste_ALL_IDB2/ALL')
-base.load()
-file = open( 'values.csv', 'w')
-iteration = 1
-for image in base.train_images:
-	segmented_image = SmartSegmentation(image.path).process()
-	gray_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)
+def get_statistics_features(rgb_image):
+	gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
 	reshaped_image = gray_image.reshape(-1)
 	clean_image = reshaped_image[reshaped_image != 0]
 	mean = 0
+	median = 0
+	standard_deviation = 0
 	if len(clean_image) > 0:
 		mean = int(np.mean(clean_image))
 		median = int(np.median(clean_image))
 		standard_deviation = np.std(clean_image)
 	else:
 		mean = 0
-	#print('mean' + str(mean) + ' | len : ' + str(len(clean_image)))
-	#if mean :
-	#	mean = int(mean)
-	#else:
-	#	mean = 0
+	return mean , median , standard_deviation
+
+
+def min_max(values , minimum = 0, maximum = 1):
+	values = [[float(j) for j in column] for column in values] #converto os valores para float
+	normalized_values = []
+	for column in values:
+		bigger = max(column) #pega o maior valor da coluna
+		lower = min(column)  #pega o menor valor da coluna
+		normalized_column = []
+		for value in column:
+			normalized_value = minimum + ((value - lower) / (bigger - lower)) * (maximum - minimum)
+			normalized_column.append(normalized_value)
+		normalized_values.append(normalized_column)
+	return normalized_values
+
+'''
+base = BaseLoader(train_base_path = 'bases/ALL' ,  validation_base_path = 'bases/Teste_ALL_IDB2/ALL')
+base.load()
+file = open( 'values.csv', 'w')
+iteration = 1
+for image in base.train_images:
+	segmented_image = SmartSegmentation(image.path).process()
+	mean , median , standard_deviation = get_statistics_features(segmented_image)
 	file.write(str(mean) + ',' + str(median) + ',' + str(standard_deviation) + ',' + str(image.label) + '\n')
 	ProgressBar().printProgress(iteration , len(base.train_images) , prefix = "Progresso : ")
 	iteration += 1
-	#print(int(np.mean(clean_image)))
-	#cv2.imshow('imagem_segmentada' , segmented_image)
-	#cv2.imshow('gray_image' , gray_image)
-	#cv2.waitKey(0)
-
 file.close()
+'''
+
+
+
+
+
+
+
+
+
+
+
+values = ([900 , 800 , 600 , 550 , 755] , [7,5,2,5,7] , [12, 10 , 12 , 10 , 10] , [-3 , 0 , 0 , -1 , 2])
+normalized_values = min_max(values , maximum = 10)
+print(normalized_values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
