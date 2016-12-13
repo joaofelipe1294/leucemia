d51ef571b1ac7036c76ad5c1ec2139599ba35e4b
Joao Felipe Lopes <joaofelipe1294@gmail.com>
2016-12-13 19:42:29 -0200

    Concluido loop que extrai as caracteristicas de cada um dos folds

--- random_tests.py
+++ random_tests.py
@@ -8,65 +8,35 @@ from modules.image_processing.filters import *
 from modules.features.features import Features
 from modules.image_processing.segmentation import SegmentNucleus
 from modules.features.features import MinMax
-
-
-
-#treinamento
-base = BaseLoader(train_base_path = 'bases/teste/treino' ,  validation_base_path = 'bases/teste/validacao')
-base.load()
-file = open( 'treino.csv', 'w')
-iteration = 1
-data = []
-for image in base.train_images:
-	rgb_image = cv2.imread(image.path)
-	segmented_image = SmartSegmentation(rgb_image).process()
-	features = Features(segmented_image)
-	mean = features.mean()
-	median = features.median()
-	standard_deviation = features.standard_deviation()
-	cell_proportion = features.nucleus_proportion(rgb_image)
-	#data.append([mean , median , standard_deviation , cell_proportion , image.label])
-	data.append([mean , median , standard_deviation , cell_proportion , image.label])
-	ProgressBar().printProgress(iteration , len(base.train_images) , prefix = "Treinamento : ")
-	iteration += 1
-normalized_data = MinMax(data).normalize(maximum = 10) #min_max(data , maximum = 10)
-for value in normalized_data:
-	#file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + '\n')
-	file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + ',' + str(value[4]) + '\n')
-file.close()
-
-
-file = open( 'validacao.csv', 'w')
-iteration = 1
-data = []
-for image in base.validation_images:
-	rgb_image = cv2.imread(image.path)
-	segmented_image = SmartSegmentation(rgb_image).process()
-	features = Features(segmented_image)
-	mean = features.mean()
-	median = features.median()
-	standard_deviation = features.standard_deviation()
-	cell_proportion = features.nucleus_proportion(rgb_image)
-	#data.append([mean , median , standard_deviation , cell_proportion , image.label])
-	data.append([mean , median , standard_deviation , cell_proportion,  image.label])
-	ProgressBar().printProgress(iteration , len(base.train_images) , prefix = "Validacao : ")
-	iteration += 1
-normalized_data = MinMax(data).normalize(maximum = 10) #min_max(data , maximum = 10)
-for value in normalized_data:
-	#file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + '\n')
-	file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + ',' + str(value[4]) + '\n')
-file.close()
+import os
 
 
 
 
 
-
-
-
-
-
-
-
-
+base_path = 'bases/k_fold'
+folds = [f for f in sorted(os.listdir(base_path))]
+for fold in folds:
+	base = BaseLoader(train_base_path = base_path + '/' + fold ,  validation_base_path = 'bases/teste/validacao')
+	base.load()
+	file = open( fold + '.csv', 'w')
+	iteration = 1
+	data = []
+	for image in base.train_images:
+		rgb_image = cv2.imread(image.path)
+		segmented_image = SmartSegmentation(rgb_image).process()
+		features = Features(segmented_image)
+		mean = features.mean()
+		median = features.median()
+		standard_deviation = features.standard_deviation()
+		cell_proportion = features.nucleus_proportion(rgb_image)
+		#data.append([mean , median , standard_deviation , cell_proportion , image.label])
+		data.append([mean , median , standard_deviation , cell_proportion , image.label])
+		ProgressBar().printProgress(iteration , len(base.train_images) , prefix = fold + " : ")
+		iteration += 1
+	#normalized_data = MinMax(data).normalize(maximum = 10) #min_max(data , maximum = 10)
+	for value in data:
+		#file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + '\n')
+		file.write(str(value[0]) + ',' + str(value[1]) + ',' + str(value[2]) + ',' + str(value[3]) + ',' + str(value[4]) + '\n')
+	file.close()
 
